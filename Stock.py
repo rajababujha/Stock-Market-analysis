369c6c65cabed43f40fb5311a3484ee037eec4e0
Raja <44722109+rajababujha@users.noreply.github.com>
2018-11-05 10:40:05 +0530

    Add files via upload

---
+++ Stock.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import quandl
+from sklearn import cross_validation,preprocessing
+from sklearn.linear_model import LinearRegression
+import math
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
+
+style.use('ggplot')
+
+df = quandl.get("WIKI/GOOGL", authtoken='g2i7bUNr-zPCD3M5XDy6')
+##print(df.head(5))
+
+df = df[['Adj. Open','Adj. Close','Adj. High','Adj. Low','Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+##print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace = True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
+
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set,confidence,forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
